804c933dcb4d0f889dd926accad20acae36dc68b
Chandra Dutt <chandradutt.chandu@gmail.com>
2021-03-17 11:55:59 +0530

    Resilience with test cases is completed

--- application/src/main/java/com/sap/cloud/sdk/tutorial/BusinessPartnerServlet.java
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/BusinessPartnerServlet.java
@@ -13,13 +13,10 @@ import java.io.IOException;
 import java.util.List;
 
 import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
-import com.sap.cloud.sdk.datamodel.odata.client.exception.ODataException;
-import com.sap.cloud.sdk.datamodel.odata.helper.Order;
 
-import com.sap.cloud.sdk.s4hana.connectivity.DefaultErpHttpDestination;
 import com.sap.cloud.sdk.s4hana.connectivity.ErpHttpDestination;
+import com.sap.cloud.sdk.s4hana.connectivity.DefaultErpHttpDestination;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
-import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultBusinessPartnerService;
 
 @WebServlet("/businesspartners")
 public class BusinessPartnerServlet extends HttpServlet {
@@ -27,40 +24,23 @@ public class BusinessPartnerServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(BusinessPartnerServlet.class);
 
-    private static final String CATEGORY_PERSON = "1";
-    // TODO: uncomment the lines below and insert your API key, if you are using the sandbox service
-    private static final String APIKEY_HEADER = "apikey";
-    private static final String SANDBOX_APIKEY = "kV0FRFj0xGhhK1sYYYq8gEEswBRGjceh";
-    private final ErpHttpDestination destination = DestinationAccessor.getDestination("MyErpSystem").asHttp().decorate(DefaultErpHttpDestination::new);
+    private static final String DESTINATION_NAME = "MyErpSystem";
 
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
         try {
+            final ErpHttpDestination destination = DestinationAccessor.getDestination(DESTINATION_NAME)
+                    .asHttp().decorate(DefaultErpHttpDestination::new);
             final List<BusinessPartner> businessPartners =
-                    new DefaultBusinessPartnerService()
-                            .getAllBusinessPartner()
-                            .select(BusinessPartner.BUSINESS_PARTNER,
-                                    BusinessPartner.LAST_NAME,
-                                    BusinessPartner.FIRST_NAME,
-                                    BusinessPartner.IS_MALE,
-                                    BusinessPartner.IS_FEMALE,
-                                    BusinessPartner.CREATION_DATE)
-                            .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
-                            .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
-                            .top(200)
-                            // TODO: uncomment the line below, if you are using the sandbox service
-                             .withHeader(APIKEY_HEADER, SANDBOX_APIKEY)
-                            .executeRequest(destination);
-
-            logger.info(String.format("Found %d business partner(s).", businessPartners.size()));
-
+                    new GetBusinessPartnersCommand(destination).execute();
             response.setContentType("application/json");
             response.getWriter().write(new Gson().toJson(businessPartners));
-        } catch (final ODataException e) {
+        } catch (final Exception e) {
             logger.error(e.getMessage(), e);
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             response.getWriter().write(e.getMessage());
+            e.printStackTrace(response.getWriter());
         }
     }
 }
--- integration-tests/src/test/java/com/sap/cloud/sdk/tutorial/BusinessPartnerServletTest.java
+++ integration-tests/src/test/java/com/sap/cloud/sdk/tutorial/BusinessPartnerServletTest.java
@@ -3,6 +3,8 @@ package com.sap.cloud.sdk.tutorial;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
+import io.vavr.control.Try;
+import org.hamcrest.Matchers;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -11,12 +13,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.net.URL;
 
+import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultDestination;
+import com.sap.cloud.sdk.cloudplatform.connectivity.Destination;
+import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
 import com.sap.cloud.sdk.testutil.MockDestination;
 import com.sap.cloud.sdk.testutil.MockUtil;
 
@@ -25,7 +28,11 @@ import static io.restassured.RestAssured.when;
 @RunWith(Arquillian.class)
 public class BusinessPartnerServletTest {
     private static final MockUtil mockUtil = new MockUtil();
-    private static final Logger logger = LoggerFactory.getLogger(BusinessPartnerServletTest.class);
+    private static final JsonSchemaValidator jsonValidator_List = JsonSchemaValidator
+            .matchesJsonSchemaInClasspath("businesspartners-schema.json");
+
+    private static final String DESTINATION_NAME = "MyErpSystem";
+    private static final Destination dummyDestination = DefaultDestination.builder().property("name", DESTINATION_NAME).property("URL", "foo").build();
 
     @ArquillianResource
     private URL baseUrl;
@@ -47,20 +54,28 @@ public class BusinessPartnerServletTest {
 
     @Test
     public void testService() {
-        // mock OData service
         // TODO: insert your service URL down below
-        mockUtil.mockDestination(MockDestination.builder("MyErpSystem", URI.create("http://localhost:3000")).build());
-
-        // JSON schema validation from resource definition
-        final JsonSchemaValidator jsonValidator = JsonSchemaValidator
-                .matchesJsonSchemaInClasspath("businesspartners-schema.json");
-
+        mockUtil.mockDestination(MockDestination.builder(DESTINATION_NAME, URI.create("http://localhost:3000")).build());
         // HTTP GET response OK, JSON header and valid schema
         when()
                 .get("/businesspartners")
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body(jsonValidator);
+                .body(jsonValidator_List);
+    }
+
+    @Test
+    public void testWithFallback() {
+        // Simulate a failed VDM call with non-existent destination
+        DestinationAccessor.setLoader((n, o) -> Try.success(dummyDestination));
+
+        // Assure an empty list is returned as fallback
+        when()
+                .get("/businesspartners")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("", Matchers.hasSize(0));
     }
 }
